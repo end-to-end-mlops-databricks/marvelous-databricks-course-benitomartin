fe573fdc6ecde369816fdae38f2269c763a13d2f
benitomartin <benitomartin>
2024-11-16 14:43:36 +0000

    Added spark session

--- notebooks/create_source_data/create_source_data_notebook.py
+++ notebooks/create_source_data/create_source_data_notebook.py
@@ -2,8 +2,13 @@
 # Databricks notebook source
 import pandas as pd
 import numpy as np
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import current_timestamp, to_utc_timestamp
 from credit_default.utils import load_config
+
+# COMMAND ----------
+
+spark = SparkSession.builder.getOrCreate()
 
 # COMMAND ----------
 
